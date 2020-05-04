666e6539b06eccb7807c0cba26c4e1fb7ed193d1
MijanurRahman40 <mijanurrahman31416@gmail.com>
2020-05-04 10:39:32 +0600

    add services

--- app/src/main/java/com/example/rentage_admin/acitvities/AddServiceActivity.java
+++ app/src/main/java/com/example/rentage_admin/acitvities/AddServiceActivity.java
@@ -1,28 +1,49 @@
 package com.example.rentage_admin.acitvities;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.rentage_admin.R;
 import com.example.rentage_admin.models.CategoryModel;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class AddServiceActivity extends AppCompatActivity {
 
     EditText titleService, descService, priceService;
     DatabaseReference databaseReference;
     String title, description, price;
-    Button submitButton;
+    Button submitButton,chooseImage;
+    String storagePermission[];
+    ImageView showImage;
+
+    private static final int STORAGE_REQUEST_CODE = 400;
+    private static final int IMAGE_PICK_GALLERY_CODE = 1000;
+    // storage
+    private StorageReference storageReference;
+    private Uri image_uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +55,23 @@ public class AddServiceActivity extends AppCompatActivity {
         priceService = findViewById(R.id.priceService);
 
         submitButton = findViewById(R.id.serviceBtn);
+        chooseImage = findViewById(R.id.chooseImageButton);
+        showImage = findViewById(R.id.showImage);
         databaseReference = FirebaseDatabase.getInstance().getReference();
 
+        storageReference = FirebaseStorage.getInstance().getReference(); // firebase storage reference
+
+
+        //storage permission
+        storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+        chooseImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                checkPermissionGranted();
+            }
+        });
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -43,35 +79,131 @@ public class AddServiceActivity extends AppCompatActivity {
                 description = descService.getText().toString();
                 price = priceService.getText().toString();
 
-                DatabaseReference dbRef = databaseReference.child("Services").push();
-                String key = dbRef.getKey();
+                final DatabaseReference dbRef = databaseReference.child("Services").push();
+                final String key = dbRef.getKey();
 
-                CategoryModel categoryModel = new CategoryModel(key, description, title,
-                        "categories");
+                StorageReference reference = storageReference.child(System.currentTimeMillis() + "." + getFileExtension(image_uri));
+
+                if (image_uri != null) {
+                    reference.putFile(image_uri)
+                            .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                @Override
+                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                    // Get a URL to the uploaded content
+                                    Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
+                                    while (!uriTask.isSuccessful()) ;
+                                    Uri downloadUri = uriTask.getResult();
+
+                                    //getting image url
+                                    CategoryModel categoryModel = new CategoryModel(key, description, title,
+                                            downloadUri.toString());
+
+                                    dbRef.setValue(categoryModel).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            Intent intent = new Intent(getApplicationContext(),
+                                                    ServicesActivity.class);
+                                            intent.putExtra("category", "Manage Category");
+                                            startActivity(intent);
+                                            Toast.makeText(AddServiceActivity.this, "Data uploaded successfully",
+                                                    Toast.LENGTH_SHORT).show();
+                                        }
+                                    }).addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Toast.makeText(AddServiceActivity.this, "Data upload failed!" + e.toString(),
+                                                    Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
 
 
-                dbRef.setValue(categoryModel).addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Intent intent = new Intent(getApplicationContext(),
-                                ServicesActivity.class);
-                        intent.putExtra("category","Manage Category");
-                        startActivity(intent);
-                        Toast.makeText(AddServiceActivity.this, "Data uploaded successfully",
-                                Toast.LENGTH_SHORT).show();
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(AddServiceActivity.this, "Data upload failed!" + e.toString(),
-                                Toast.LENGTH_SHORT).show();
-                    }
-                });
+                                    Toast.makeText(AddServiceActivity.this, "Profile updated successfully..", Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception exception) {
+                                    // Handle unsuccessful uploads
+                                    Toast.makeText(AddServiceActivity.this, "" + exception.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                }
+
+
 
             }
         });
 
-
-
     }
+
+    private void checkPermissionGranted() {
+        if (!checkStoragePermission()) {
+            //Storage permission not allowed
+            requestStoragePermission();
+        } else {
+            //permission allowed,take picture
+            pickGallery();
+        }
+    }
+
+    private void pickGallery() {
+        //intent to pick image from gallery
+        Intent intent = new Intent(Intent.ACTION_PICK);
+
+        //set intent type to image
+        intent.setType("image/*");
+        startActivityForResult(intent, IMAGE_PICK_GALLERY_CODE);
+    }
+
+    private void requestStoragePermission() {
+        ActivityCompat.requestPermissions(this, storagePermission, STORAGE_REQUEST_CODE);
+    }
+
+    private boolean checkStoragePermission() {
+        boolean result = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+        return result;
+    }
+
+    //Handle Permission result
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case STORAGE_REQUEST_CODE:
+                if (grantResults.length > 0) {
+                    boolean writeStorageAccepted = grantResults[0] ==
+                            PackageManager.PERMISSION_GRANTED;
+                    if (writeStorageAccepted) {
+                        pickGallery();
+                    } else {
+                        Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+        }
+    }
+
+    //Handle Image result
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+
+            if (requestCode == IMAGE_PICK_GALLERY_CODE) {
+                image_uri = data.getData();
+                showImage.setImageURI(image_uri);
+                showImage.setVisibility(View.VISIBLE);
+                showImage.invalidate();
+            }
+        }
+    }
+
+    //get image extension
+    public String getFileExtension(Uri imageUrl) {
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(imageUrl));
+    }
+
 }
